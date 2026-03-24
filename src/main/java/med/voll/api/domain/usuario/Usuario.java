ad5215833b82dc659ef761545086e561496e7db3
robertobzrr <robertobezerrafh@outlook.com>
2026-03-24 19:56:20 -0300

    patch rapido

---
+++ src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -0,0 +1,25 @@
+package med.voll.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuarios")
+
+//não tem setter para não da permissão do usuario poder mudar a senha.
+//ele pode ver mas bastar bloquear a visualização
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+}
